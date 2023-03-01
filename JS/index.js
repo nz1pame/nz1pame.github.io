1563b97ecc75f76f8107904bc4e5b98a4d0b66cf
nz1pame <124739078+nz1pame@users.noreply.github.com>
2023-03-01 17:01:28 -0300

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+document.getElementById('boton1')
+.addEventListener('click',function(){
+console.log("Telefono desde EvenListener");
+document.getElementById("demo").innerHTML="(318) 325-5726"
+
+});  
+document.getElementById('boton2')
+.addEventListener('click',function(){
+console.log("Dirección desde EvenListener");
+document.getElementById("demo1").innerHTML="6259 Valley View Ln"
+
+});  
+document.getElementById('boton3')
+.addEventListener('click',function(){
+console.log("E-mail desde EvenListener");
+document.getElementById("demo2").innerHTML="zoemayers@example.com"
+
+});  
+
+
+
+
+    
